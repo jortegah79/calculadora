3dfbf5c71a8ea401860e70484e868e48021e7da1
juan <jortegah79@gmail.com>
2022-02-26 12:38:40 +0100

    finalizado a la espera de ver que pasa con el icono de la pestaña

--- src/index.js
+++ src/index.js
@@ -2,109 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-const valorTeclas = [
-  {
-    texto: 'CLEAR',
-    id: "clear",
-    clases: 'tecla col-2 operacion'
-  },
-  {
-    texto: '/',
-    id: "divide",
-    clases: 'tecla operacion'
-  },
-  {
-    texto: '*',
-    id: "multiply",
-    clases: 'tecla  operacion'
-
-  },
-  {
-    texto: '7',
-    id: "seven",
-    clases: 'tecla inactiva'
-
-  },
-  {
-    texto: '8',
-    id: "eight",
-    clases: 'tecla inactiva'
-
-  },
-  {
-    texto: '9',
-    id: "nine",
-    clases: 'tecla inactiva'
-
-  },
-  {
-    texto: '-',
-    id: "subtract",
-    clases: 'tecla operacion'
-
-  },
-  {
-    texto: '4',
-    id: "four",
-    clases: 'tecla inactiva'
-
-  },
-  {
-    texto: '5',
-    id: "five",
-    clases: 'tecla inactiva'
-
-  },
-  {
-    texto: '6',
-    id: "six",
-    clases: 'tecla inactiva'
-
-  },
-  {
-    texto: '+',
-    id: "add",
-    clases: 'tecla  operacion'
-
-  },
-  {
-    texto: '1',
-    id: "one",
-    clases: 'tecla inactiva'
-
-  },
-  {
-    texto: '2',
-    id: "two",
-    clases: 'tecla inactiva'
-
-  },
-  {
-    texto: '3',
-    id: "three",
-    clases: 'tecla inactiva'
-
-  },
-  {
-    texto: '=',
-    id: "equals",
-    clases: 'tecla row-2  operacion'
-  },
-  {
-    texto: '0',
-    id: "zero",
-    clases: 'tecla col-2 inactiva'
-
-  },
-  {
-    texto: '.',
-    id: "decimal",
-    clases: 'tecla inactiva'
-
-  }
-]
-
 const Pantalla = props => {
   return (
     <div id="pantalla">
@@ -113,8 +10,8 @@ const Pantalla = props => {
   );
 }
 const Teclado = props => {
-  const tec = valorTeclas.map(function (x, i) {
-    return (<li key={i} onClick={props.handleClick} id={x.id} className={x.clases}>{x.texto}</li>)
+  const tec = props.tecla.map(function (x, i) {
+    return (<li key={i} onClick={x.click} id={x.id} className={x.clases}>{x.texto}</li>)
   });
   return (
     <div className='teclado'>
@@ -127,85 +24,224 @@ class Calculadora extends React.Component {
     super(props);
     this.state = {
       operacion: [],
-      txtResultado: '0'
+      resultado: '0',
+      operacionAritmetica: [],
+      resFinal : false
     }
-    this.handleClick = this.handleClick.bind(this);
+    this.handleClickNum = this.handleClickNum.bind(this);
+    this.handleClickOps = this.handleClickOps.bind(this);
+    this.handleClickClr = this.handleClickClr.bind(this);
+    this.handleClickEq = this.handleClickEq.bind(this);
+    this.compruebaComa=this.compruebaComa.bind(this);
+    this.compruebaSignos=this.compruebaSignos.bind(this);
   }
+ 
+  /*************************************************control de clear                ********* */
+  handleClickClr(e) {
+    let boton = document.getElementById(e.target.id);
+    boton.className += ' operactiva ';
+    setTimeout(() => {
+      boton.className = ' tecla operacion col-2';
+    }, 100);
+    this.setState({
+      resultado: '0',
+      operacion: [],
+      operacionAritmetica: [],
+      resFinal : false
+    }); 
+  }
+
   /*************************************************** aqui empieza el control de teclas... */
-  handleClick(e) {
-    
+  handleClickNum(e) {
+    let resultado;
+    if(this.state.resFinal){
+      resultado='0'
+    }else{
+      resultado = this.state.resultado;
+      //numero guardado en estado
+    }
+    let operacion=this.compruebaSignos(this.state.operacion);
     let boton = document.getElementById(e.target.id);
     boton.className += ' activa ';
+    //boton pulsado
     let valor = e.target.innerText;
-    let resultado = this.state.txtResultado;
-    let operacion = this.state.operacion;
-    if (valor === 'CLEAR') {
-      resultado = '0';
-      operacion = [];
-      setTimeout(() => {
-        boton.className = ' tecla operacion col-2';
-      }, 100)
-    } else if (+valor >= 1 && +valor <= 9) {
+    //caracter que activa el boton  
+    if (valor >= 0 && valor <= 9) {
       resultado = resultado === '0' ? valor : resultado + valor;
       setTimeout(() => {
-        boton.className = ' tecla inactiva';
-      }, 100)
-    } else if (valor === '0') {
-      resultado = resultado === '0' ? resultado = valor : resultado + valor;
-      setTimeout(() => {
-        boton.className = ' tecla inactiva col-2';
-      }, 100)
-    } else if (valor === ('.')) {
-      resultado = resultado.includes('.') ? resultado : resultado + '.';
+        boton.className = valor === '0' ? ' tecla inactiva col-2' : ' tecla inactiva';
+      }, 100);
+    } else if (valor === '.') {
+      resultado = resultado.includes('.') ? resultado : resultado + '.';     
       setTimeout(() => {
         boton.className = ' tecla inactiva';
       }, 100);
     }
     this.setState({
-      txtResultado: resultado,
-      operacion: operacion
-    })
-
-
-    if (valor === '+' || valor === '-' || valor === '*' || valor === '/') {
-      setTimeout(() => {
-        boton.className = ' tecla operacion';
-      }, 100)
-      this.setState(
-        {
-          operacion: [...this.state.operacion, resultado, valor],
-          txtResultado: '0'
-        }
-      )
-    }
-    if (valor === '=') {
-      setTimeout(() => {
-        boton.className = ' tecla operacion row-2';
-      }, 100)
-      resultado=[...this.state.operacion,resultado].join("");
-      // eslint-disable-next-line no-eval
-      let valor=eval(resultado);
-      this.setState(
-        {
-          operacion: [],
-          txtResultado: valor
-      }
-      )      
-    }
+      resultado: resultado,
+      operacionAritmetica: this.state.resultado.length===1?[...this.state.operacionAritmetica,operacion]:this.state.operacionAritmetica,
+      operacion:[],
+      resFinal : false    
+    });
+    //manejamos solo el resultado. 
   }
+  /****************************************************************control de =                */
+  handleClickEq(e) {
+    let boton = document.getElementById(e.target.id);
+    boton.className += ' operactiva ';   
+    setTimeout(() => {
+      boton.className = ' tecla operacion row-2';
+    }, 100)
+    let resultado= parseFloat(this.state.resultado);
+    let newResultado = [...this.state.operacionAritmetica, resultado].join('');
+    // eslint-disable-next-line no-eval
+    let valor = eval(newResultado);
+    this.setState(
+      {
+        operacion: [],
+        resultado: valor,       
+        operacionAritmetica:[],
+        resFinal :true
+      }
+    )  
+  }
+  /**********************************************control de operaciones ********************** */
+  handleClickOps(e) {
+    let boton = document.getElementById(e.target.id);
+    boton.className += ' operactiva ';
+    setTimeout(()=>{
+      boton.className = ' tecla operacion';
+    }, 100);
+    this.setState({
+       resultado: '0',
+       operacionAritmetica:this.state.operacion.length===0?[...this.state.operacionAritmetica, parseFloat(this.state.resultado)]:[...this.state.operacionAritmetica],
+       operacion: [...this.state.operacion,e.target.innerText],
+       resFinal:false
+      });
+    }
+  compruebaSignos(arr){
+    let arr2=arr[arr.length-1] !== '-'?arr.slice(-1):arr.slice(-2);    
+    return arr2.join('');
+ }
+ compruebaComa(x){
+   let arr=x.split("");
+   return arr.some(z=>z==='.')
+ }
   render() {
-
+    const valorTeclas = [
+      {
+        texto: 'CLEAR',
+        id: "clear",
+        clases: 'tecla col-2 operacion',
+        click: this.handleClickClr
+      },
+      {
+        texto: '/',
+        id: "divide",
+        clases: 'tecla operacion',
+        click: this.handleClickOps
+      },
+      {
+        texto: '*',
+        id: "multiply",
+        clases: 'tecla  operacion',
+        click: this.handleClickOps
+      },
+      {
+        texto: '7',
+        id: "seven",
+        clases: 'tecla inactiva',
+        click: this.handleClickNum
+      },
+      {
+        texto: '8',
+        id: "eight",
+        clases: 'tecla inactiva',
+        click: this.handleClickNum
+      },
+      {
+        texto: '9',
+        id: "nine",
+        clases: 'tecla inactiva',
+        click: this.handleClickNum
+      },
+      {
+        texto: '-',
+        id: "subtract",
+        clases: 'tecla operacion',
+        click: this.handleClickOps
+      },
+      {
+        texto: '4',
+        id: "four",
+        clases: 'tecla inactiva',
+        click: this.handleClickNum
+      },
+      {
+        texto: '5',
+        id: "five",
+        clases: 'tecla inactiva',
+        click: this.handleClickNum
+      },
+      {
+        texto: '6',
+        id: "six",
+        clases: 'tecla inactiva',
+        click: this.handleClickNum
+      },
+      {
+        texto: '+',
+        id: "add",
+        clases: 'tecla  operacion',
+        click: this.handleClickOps
+      },
+      {
+        texto: '1',
+        id: "one",
+        clases: 'tecla inactiva',
+        click: this.handleClickNum
+      },
+      {
+        texto: '2',
+        id: "two",
+        clases: 'tecla inactiva',
+        click: this.handleClickNum
+      },
+      {
+        texto: '3',
+        id: "three",
+        clases: 'tecla inactiva',
+        click: this.handleClickNum
+      },
+      {
+        texto: '=',
+        id: "equals",
+        clases: 'tecla row-2  operacion',
+        click: this.handleClickEq
+      },
+      {
+        texto: '0',
+        id: "zero",
+        clases: 'tecla col-2 inactiva',
+        click: this.handleClickNum
+      },
+      {
+        texto: '.',
+        id: "decimal",
+        clases: 'tecla inactiva',
+        click: this.handleClickNum
+      }
+    ]
     return (
       <div id='App'>
         <h1 className="blanco centrado">Calculadora by J.Ortega</h1>
         <div className="calculadora blanco">
           <h3 >CASIO</h3>
-          <Pantalla operaciones={this.state.txtOperaciones} resultado={this.state.txtResultado} />
-          <Teclado handleClick={this.handleClick} />
+          <Pantalla resultado={this.state.resultado} />
+          <Teclado tecla={valorTeclas} />
         </div>
       </div>
     );
   }
 }
-
 ReactDOM.render(<Calculadora />, document.getElementById('root'));
